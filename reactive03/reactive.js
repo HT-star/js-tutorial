e0a8d3adadbfe5f7b10d7312f122318f497b9f2e
Takaaki Hoshi <hoshi5017@gmail.com>
2022-03-29 22:33:29 +0900

    done reactive03

---
+++ reactive03/reactive.js
@@ -0,0 +1,36 @@
+
+const handler = {
+    get: function(target, prop, receiver) {
+        // 渡されたオブジェクトのキーの値を返す
+        const res = Reflect.get(target, prop, receiver)
+        return res
+    },
+    set: function(target, prop, value, receiver){
+        // target = オブジェクト名
+        // prop = プロパティ名
+        // value= 代入された値
+
+        // Reflect.setで、 渡されたオブジェクトのキーの値に値を設定
+        const res = Reflect.set(target, prop, value, receiver)
+        trigger()
+        return res
+    }
+};
+
+function reactive(target){
+    return new Proxy(target, handler)
+}
+
+let activeEffect = null
+function effect(fn){
+    // console.logを設定
+    activeEffect = fn;
+}
+
+function trigger(){
+    // index.htmlで呼び出されたeffectにconsole.logが渡り、activeEffectに設定
+    // activeEffectにセットされたconsole.logを実行
+    activeEffect()
+}
+
+export { reactive, effect}
